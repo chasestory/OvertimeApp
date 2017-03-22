9edf68835023bad6c640df6e091d729b84d0046f
Chase Story <chasestory@outlook.com>
2017-03-22 16:18:22 -0600

    Implemented styles and layout for posts page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,10 @@
+@user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
+
+puts "1 User created"
+
+
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content")
+  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user)
 end
 
 puts "100 Posts that have been created"
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,18 +1,27 @@
 require 'rails_helper'
 
 describe 'navigate' do
- 
+  
   describe 'index' do
-    it 'can be reached successfully' do
+    before do 
       visit posts_path
+    end
+    it 'can be reached successfully' do
       expect(page.status_code).to eq(200)
     end
 
     it 'has a title of Posts' do
-      visit posts_path
       expect(page).to have_content(/Posts/)
     end
-  end  
+
+    it 'has a list of posts' do
+      user = User.create(email: "test@test.com", password: "123456", password_confirmation: "123456", first_name: "jon", last_name: "doe")
+      post1 = Post.create(date: Date.today, rationale: "Post1", user_id: user.id)
+      post2 = Post.create(date: Date.today, rationale: "Post2", user_id: user.id)
+      visit posts_path
+      expect(page).to have_content(/Posts1|Post2/)
+   end  
+  end
 
   describe 'creation' do 
     before do
