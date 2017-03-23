0170dae2227d4dab079b8ad15133c38c0d4ca928
Chase Story <chasestory@outlook.com>
2017-03-22 22:06:08 -0600

    Refactored index page to use Post partial

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -19,7 +19,7 @@ describe 'navigate' do
       post1 = Post.create(date: Date.today, rationale: "Post1", user_id: user.id)
       post2 = Post.create(date: Date.today, rationale: "Post2", user_id: user.id)
       visit posts_path
-      expect(page).to have_content(/Posts1|Post2/)
+      expect(page).to have_content(/Post1|Post2/)
    end  
   end
 
