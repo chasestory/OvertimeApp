13768f713d68a178b68f767f46df75a6b0391c91
Chase Story <chasestory@outlook.com>
2017-03-30 20:42:23 -0600

    Implemented the growl notifications for permission errors

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -5,11 +5,12 @@ describe 'navigate' do
     @user = FactoryGirl.create(:user)
     login_as(@user, :scope => :user)
   end
-  
+
   describe 'index' do
-    before do 
+    before do
       visit posts_path
     end
+
     it 'can be reached successfully' do
       expect(page.status_code).to eq(200)
     end
@@ -19,13 +20,11 @@ describe 'navigate' do
     end
 
     it 'has a list of posts' do
-      user = User.create(email: "test@test.com", password: "123456", password_confirmation: "123456", first_name: "jon", last_name: "doe")
       post1 = FactoryGirl.build_stubbed(:post)
       post2 = FactoryGirl.build_stubbed(:second_post)
-      login_as(user, :scope => :user)
       visit posts_path
       expect(page).to have_content(/Rationale|content/)
-    end  
+    end
   end
 
   describe 'new' do
@@ -47,10 +46,8 @@ describe 'navigate' do
     end
   end
 
-  describe 'creation' do 
+  describe 'creation' do
     before do
-      user = User.create(email: "test@test.com", password: "123456", password_confirmation: "123456", first_name: "jon", last_name: "doe")
-      login_as(user, :scope => :user)
       visit new_post_path
     end
 
@@ -58,11 +55,11 @@ describe 'navigate' do
       expect(page.status_code).to eq(200)
     end
 
-    it 'can be created form new form page' do
+    it 'can be created from new form page' do
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: "Some rationale"
       click_on "Save"
-    
+
       expect(page).to have_content("Some rationale")
     end
 
@@ -72,30 +69,34 @@ describe 'navigate' do
       click_on "Save"
 
       expect(User.last.posts.last.rationale).to eq("User Association")
-    end     
+    end
   end
 
   describe 'edit' do
     before do
-      @post = FactoryGirl.create(:post)
-    end
-
-    it 'can be reached by clicking edit on index page' do
-      @post = FactoryGirl.create(:post)
-      visit posts_path
-
-      click_link("edit_#{@post.id}")
-      expect(page.status_code).to eq(200)
+      @edit_user = User.create(first_name: "asdf", last_name: "asdf", email: "asdfasdf@asdf.com", password: "asdfasdf", password_confirmation: "asdfasdf")
+      login_as(@edit_user, :scope => :user)
+      @edit_post = Post.create(date: Date.today, rationale: "asdf", user_id: @edit_user.id)
     end
 
     it 'can be edited' do
-      visit edit_post_path(@post)
+      visit edit_post_path(@edit_post)
 
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: "Edited content"
       click_on "Save"
 
       expect(page).to have_content("Edited content")
+    end
+
+    it 'cannot be edited by a non authorized user' do
+      logout(:user)
+      non_authorized_user = FactoryGirl.create(:non_authorized_user)
+      login_as(non_authorized_user, :scope => :user)
+
+      visit edit_post_path(@edit_post)
+
+      expect(current_path).to eq(root_path)
     end
   end
 end
