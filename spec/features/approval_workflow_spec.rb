2780a6223a4894e7e77f45666d7339969b71766e
Chase Story <chasestory@outlook.com>
2017-04-01 22:32:54 -0600

    implemented status styles for posts, also added hiding elements in the view based on pundit permissions

--- spec/features/approval_workflow_spec.rb
+++ spec/features/approval_workflow_spec.rb
@@ -18,4 +18,16 @@ describe 'edit' do
 
     expect(@post.reload.status).to eq('approved')
   end
+
+  it 'should not be editable by the post creator if status is approved' do
+    logout(:user)
+    user = FactoryGirl.create(:user)
+    login_as(user, :scope => :user)
+
+    @post.update(user_id: user.id, status: 'approved')
+
+    visit edit_post_path(@post)
+
+    expect(current_path).to eq(root_path)
+  end
 end
