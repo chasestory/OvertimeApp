0fecd56d30ea19ebaf5310b70f989acfe081300c
Chase Story <chasestory@outlook.com>
2017-04-03 15:01:08 -0600

    Implemented sms functionality, added regular expression matcher for phone numbers

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,18 +3,18 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   describe "Creation" do
     before do
-        @post = FactoryGirl.create(:post)
+      @post = FactoryGirl.create(:post)
     end
 
     it 'can be created' do  
-        expect(@post).to be_valid
+      expect(@post).to be_valid
     end
 
     it 'cannot be created without a date, rationale, and overtime_request' do
-        @post.date = nil
+      @post.date = nil
       @post.rationale = nil
-        @post.overtime_request = nil
-        expect(@post).to_not be_valid
+      @post.overtime_request = nil
+      expect(@post).to_not be_valid
     end
 
     it 'has an overtime_request greater than 0.0' do
@@ -23,5 +23,3 @@ RSpec.describe Post, type: :model do
     end
   end
 end
-
-
