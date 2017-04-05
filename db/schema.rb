341d748484f883827e16dda3873786c5179f8604
Chase Story <chasestory@outlook.com>
2017-04-05 15:45:52 -0600

    Implemented audit log features, and dashboard

--- db/schema.rb
+++ db/schema.rb
@@ -28,11 +28,11 @@ ActiveRecord::Schema.define(version: 20170403214221) do
   create_table "posts", force: :cascade do |t|
     t.date     "date"
     t.text     "rationale"
-    t.datetime "created_at",                   null: false
-    t.datetime "updated_at",                   null: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
     t.integer  "user_id"
     t.integer  "status",           default: 0
-    t.decimal  "overtime_request"
+    t.decimal  "overtime_request", default: "0.0"
     t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
   end
 
