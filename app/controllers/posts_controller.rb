0c3465dbde668113662ec231a2d75843c3c7794a
Chase Story <chasestory@outlook.com>
2017-04-06 19:14:48 -0600

    Implemented the admin homepage approve or review buttons, and added functionality for approval without routing to new page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,18 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :set_post, only: [:show, :edit, :update, :destroy, :approve]
+  
   def index
     @posts = Post.posts_by(current_user).page(params[:page]).per(10)
   end
 
-  def new
-    @post = Post.new
+  def approve
+    authorize @post
+    @post.approved!
+    redirect_to root_path, notice: "The post has been approved"
   end
 
-  def destroy
-    @post.delete
-    redirect_to posts_path, notice: 'Your post was deleted successfully'
+  def new
+    @post = Post.new
   end
 
   def create
@@ -31,14 +33,19 @@ class PostsController < ApplicationController
   def update
     authorize @post
     
-      if @post.update(post_params)
-      redirect_to @post, notice: 'Your post was created successfully'    
+    if @post.update(post_params)
+      redirect_to @post, notice: 'Your post was edited successfully'
     else
       render :edit
     end
   end
 
   def show
+  end
+
+  def destroy
+    @post.delete
+    redirect_to posts_path, notice: 'Your post was deleted successfully'
   end
 
   private
