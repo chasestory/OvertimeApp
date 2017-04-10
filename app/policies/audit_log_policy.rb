eb7dc585192ddf4bc622bde4222c48502e0bb975
Chase Story <chasestory@outlook.com>
2017-04-10 10:57:57 -0600

    Implemented confirmation radio buttons for employee homepage for audit logs, and and set if 0 pending requests no show confirmation buttons

--- app/policies/audit_log_policy.rb
+++ app/policies/audit_log_policy.rb
@@ -3,13 +3,13 @@ class AuditLogPolicy < ApplicationPolicy
     return true if admin?
   end
 
+  def confirm?
+    record.user_id == user.id
+  end
+
   private
 
     def admin?
       admin_types.include?(user.type)
-    end
-
-    def confirm?
-      record.user_id == user.id
     end
 end
